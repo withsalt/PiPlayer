f381133d94da737e30070966fadae48ac54f1599
withsalt <sysrun@163.com>
2023-10-29 22:11:25 +0800

    fix: update nuget

--- src/PiPlayer/Controllers/AudioController.cs
+++ src/PiPlayer/Controllers/AudioController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PiPlayer.Configs;
 using PiPlayer.Models.Entities;
